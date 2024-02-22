f4c1ef155892c0f6f2c716cba8d97aefd29efdda
Gabriel Moutinho <MOUTINHO@MOUTINHO.lan>
2024-02-22 20:54:45 -0300

    Modelo padrão de programação do CodeIgniter3 para projetos simples.

---
+++ application/models/PadraoModels.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PadraoModel extends CI_Model {
+
+/*
+Exemplo de consulta em DB select;
+
+public function usuarioWhereId($id){
+        $query = $this->db->get_where('usuarios', array('id' => $id))->result();
+        return $query;
+        
+        
+    } */
+
+}
